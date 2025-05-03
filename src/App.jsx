6d18cc9fb32b62e1918da603002b22b33b1f780f
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-03 16:08:08 +0200

    finished: header (mobile version)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
