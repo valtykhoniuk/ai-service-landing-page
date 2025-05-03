0265e38474323cc166587039f0b205dfb7e7924c
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-03 22:07:42 +0200

    finished: hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,18 @@ import Section from "./Section";
 import { curve, heroBackground } from "../assets";
 import { robot } from "../assets";
 import Button from "./Button";
+import { useRef } from "react";
+
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -13,14 +23,14 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div
           className="relative z-1 max-w-[62rem]
         mx-auto text-center mb-[4rem] md:mb-20 lg:mb:
         [6rem]"
         >
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with {` `}
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -68,8 +78,37 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <Generating
+                  className="absolute left-4 right-4 bottom-5 
+                md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"
+                />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul
+                    className="hidden absolute
+                    -left-[5.5rem] bottom-[7.5rem] px-1 py-1
+                     bg-n-9/40 backdrop-blur border
+                     border-n-1/10 rounded-2xl xl:flex"
+                  >
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div
             className="absolute -top-[54%] left-1/2
@@ -85,8 +124,17 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+
+          <BackgroundCircles />
         </div>
+
+        <CompanyLogos
+          className="hidden relative z-10
+        mt-20 lg:block"
+        />
       </div>
+
+      <BottomLine />
     </Section>
   );
 };
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -12,12 +12,13 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${
-        customPaddings ||
-        `py-10 lg:py-16 xl:py-20 
-        ${crosses ? "lg:py-32 xl:py-40" : ""} 
-        ${className || ""}`
-      }`}
+      className={`
+    relative 
+    ${
+      customPaddings ||
+      `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+    } 
+    ${className || ""}`}
     >
       {children}
 
